46ab6835fbe9333055d85c72eb4ccd32b52b4839
Bharath Ramsundar <bharath.ramsundar@gmail.com>
2019-06-07 10:29:07 -0700

    changes for v4-v5

--- computable/helpers/account.py
+++ computable/helpers/account.py
@@ -3,7 +3,7 @@ from computable.helpers.constants import UNLOCK_DURATION
 def lock(w3, acct=None):
     if acct is None:
         acct = w3.eth.defaultAccount
-    return w3.personal.lockAccount(acct)
+    return w3.geth.personal.lockAccount(acct)
 
 def unlock(w3, phrase, acct=None, timeout=None):
     """
@@ -15,4 +15,4 @@ def unlock(w3, phrase, acct=None, timeout=None):
         acct = w3.eth.defaultAccount
     if timeout is None:
         timeout = UNLOCK_DURATION
-    return w3.personal.unlockAccount(acct, phrase, timeout)
+    return w3.geth.personal.unlockAccount(acct, phrase, timeout)

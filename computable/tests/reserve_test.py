632a535e4ee2eda8b8d051b91948f03c48a3028f
Bharath Ramsundar <bharath.ramsundar@gmail.com>
2019-09-17 16:58:43 -0700

    bugs

--- computable/tests/reserve_test.py
+++ computable/tests/reserve_test.py
@@ -1,8 +1,18 @@
 import pytest
 import web3
 from web3 import Web3
+from computable.helpers.transaction import call
 
 def test_deploy(reserve):
     assert len(reserve.account) == 42
     assert len(reserve.address) == 42
     assert reserve.account != reserve.address
+
+def test_get_withdrawal_proceeds(reserve):
+    proceeds = call(reserve.get_withdrawal_proceeds(reserve.account))
+    assert proceeds == 0
+
+def test_withdraw(reserve):
+    _ = call(reserve.withdraw())
+    proceeds = call(reserve.get_withdrawal_proceeds(reserve.account))
+    assert proceeds == 0
